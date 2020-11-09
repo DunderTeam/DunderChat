3298797793c773f56fe05f4b84871fc5e453e147
Infroz <infroz98@gmail.com>
2020-11-09 15:38:14 +0100

    Maven added with dependency

---
+++ src/main/java/model/DB.java
@@ -0,0 +1,10 @@
+package model;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+
+
+public class DB {
+}
