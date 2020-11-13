8e480fbf5013092372ec504671e7b8ae10b9583d
Hans Kristian Hoel <hanskhoe@stud.ntnu.no>
2020-11-13 13:11:09 +0100

    Setting up functions for chatManager list

--- src/main/java/controller/chat/ChatManager.java
+++ src/main/java/controller/chat/ChatManager.java
@@ -1,17 +1,46 @@
 package controller.chat;
 
+import controller.networking.data.Message;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatManager {
+    int value1;
+    Chat value2;
 
-List<Chat> chatManager = new ArrayList<Chat>();
 
-    public void addChatToList (Chat ch){ // adds chat to list
-        chatManager.add(ch);
+    ChatManager (int nr, Chat ch){
+        value1 = nr;
+        value2 = ch;
     }
 
-    public List<Chat> getChatManager() { // returns list og chats
+    List<ChatManager> chatManager = new ArrayList<ChatManager>(); // lists of Chats
+
+    public void addChatToList (int nr,Chat ch){ // adds chat to list
+        ChatManager temp = new ChatManager(nr, ch);
+        chatManager.add(temp);
+    }
+
+    public List<ChatManager> getChatManager() { // returns list of chats
         return chatManager;
+    }
+
+    public Chat getChatById(int id){ // Gets chat from list of chats by id
+        Chat temp = null;
+        for (int i = 0; i < chatManager.size(); i++ ){
+            if (value1 == id){
+                temp = value2;
+            }
+        }
+        return temp;
+    }
+
+    public void deleteChatById(int id){ // deletes chat from list by id
+        for (int i = 0; i < chatManager.size(); i++){
+            if(value1 == id){
+                chatManager.remove(i);
+            }
+        }
     }
 }
