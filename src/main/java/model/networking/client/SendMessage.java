8fc85df1bde9f486ff9240dd72dfb644481bc169
Infroz <infroz98@gmail.com>
2020-11-13 16:00:16 +0100

    Implemented message retry send

--- src/main/java/model/networking/client/SendMessage.java
+++ src/main/java/model/networking/client/SendMessage.java
@@ -6,9 +6,12 @@ import model.networking.data.Status;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class SendMessage implements Runnable {
+    private int connectionAttempts = 5;
+
     SendMessage(String i, int p, Message m) {
         ip = i; port = p; msg = m;
         m.setTimestamp(java.time.Clock.systemUTC().instant().toString());
@@ -33,34 +36,57 @@ public class SendMessage implements Runnable {
     // Thread x = new Thread(new SendMessage()); x.start();
     public void run() {
         try {
-            conn = new Socket(ip, port); // Opens Connection
-
-            DataInputStream in = new DataInputStream(conn.getInputStream());
-            DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-
-            out.writeUTF(encodeMessage(msg));
-            out.flush();
-
-            int status = decodeStatus(in.readUTF()).getStatus();
-
-            // Gets server response, do something based on result?:
-            switch(status) {
-                case 200:
-                    // all good, message sent and received
-                    break;
-                case 204:
-                    // empty message sent
-                    break;
-                default:
-                    // Message not received, try resending
-                    // Todo: implement resend/retry functionality
-                    break;
+            if(!send()) { // if failed to send, retry
+                retry();
             }
-
-            out.close();
-            conn.close();
         } catch(Exception e) {
             System.out.println(e.getCause());
+        }
+    }
+
+    private boolean send() throws IOException {
+        conn = new Socket(ip, port); // Opens Connection
+
+        DataInputStream in = new DataInputStream(conn.getInputStream());
+        DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+
+        out.writeUTF(encodeMessage(msg));
+        out.flush();
+
+        int status = decodeStatus(in.readUTF()).getStatus();
+
+        // Gets server response, do something based on result?:
+        switch(status) {
+            case 200:
+                // all good, message sent and received
+                out.close();
+                conn.close();
+                return true;
+            case 204:
+                // empty message sent
+                // Todo: implement message check
+                return false;
+            default:
+                // Message not received, try resending
+                out.close();
+                conn.close();
+                return false;
+        }
+    }
+
+    private void retry() {
+        while(connectionAttempts > 0) {
+            try {
+                Thread.currentThread().wait(500);
+                System.out.println("Message failed to send, retrying...");
+                if(send()) { // If successful, then break loop
+                    break;
+                }
+                connectionAttempts--;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                break;
+            }
         }
     }
 }
