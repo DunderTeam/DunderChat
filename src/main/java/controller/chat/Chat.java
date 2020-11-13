d98de7acb1d01f8bc85d14b22066b6a12107714a
Hans Kristian Hoel <hanskhoe@stud.ntnu.no>
2020-11-13 11:41:04 +0100

    Setting up functions for hadeling lists of chats

--- src/main/java/controller/chat/Chat.java
+++ src/main/java/controller/chat/Chat.java
@@ -1,5 +1,19 @@
 package controller.chat;
 
+import controller.networking.data.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chat {
-    
+
+    List<Message> chat = new ArrayList<Message>(); // Creating list of all messages between two people
+
+    public void addMessageToList (Message mes) { // Adds new message to list
+        chat.add(mes);
+    }
+
+    public List<Message> getChat() { // returns list
+        return chat;
+    }
 }
--- src/main/java/controller/chat/ChatManager.java
+++ src/main/java/controller/chat/ChatManager.java
@@ -1,7 +1,17 @@
 package controller.chat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatManager {
 
+List<Chat> chatManager = new ArrayList<Chat>();
 
+    public void addChatToList (Chat ch){ // adds chat to list
+        chatManager.add(ch);
+    }
 
+    public List<Chat> getChatManager() { // returns list og chats
+        return chatManager;
+    }
 }
