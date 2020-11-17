33bd68d4c268485a302c0f36a099a1d1ccb78e1a
jesperu7 <jesperu7@hotmail.com>
2020-11-17 13:14:36 +0100

    cleaned up files, added more adatabase methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,14 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.*;
 import model.database.DB;
-import model.database.Encryption;
-import model.networking.client.Connection;
-import model.networking.client.ConnectionManager;
-import model.networking.data.Message;
-import model.networking.server.Server;
 import org.bson.Document;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-        // connect to mongodb, get a client from the method
-        MongoClient mongoClient = DB.connect();
         // get user collection from mongodb
         MongoCollection<Document> userCollection = DB.getUserCollection();
 
-
+        /*
         Thread server = new Thread(new Server(25));
         server.start();
 
@@ -28,22 +20,29 @@ public class Main {
         msg.setName("John Wick");
 
         manager.sendMessage(manager.connections.get(0), msg);
+         */
 
         // variables used for testing input to database before buttons are added/simulating registering new users
-        String username = "bruker4";
-        String password = "qwerty";
-
-        // encrypt the password using simple hash
-        String encryptedPassword = Encryption.encryptPassword(password);
+        String username = "bruker1";
+        String ip = "123";
+        String password = "123456";
+        String newPassword = "password";
 
         // add new user to data base
-        //DB.addUser(userCollection, username, encryptedPassword);
+        //DB.addUser(userCollection, username, ip, password);
 
         // login
-        DB.login(userCollection, username, encryptedPassword);
+        DB.login(userCollection, username, password);
 
         // delete user
-        //DB.deleteUser(userCollection, username, encryptedPassword);
+        //DB.deleteUser(userCollection, username, password);
+
+        // change password
+        //DB.changePassword(userCollection, username, password, newPassword);
+
+
+
+
 
     }
 }
--- src/main/java/model/database/DB.java
+++ src/main/java/model/database/DB.java
@@ -3,13 +3,13 @@ package model.database;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
 
 public class DB {
 
     // connect to mongodb
     private static final String connection = "mongodb+srv://dunderuser:JNKwmvZ56SPAGMQi@dunderteam.v2fut.mongodb.net/test?retryWrites=true&w=majority";
-    private static final MongoClient mongoClient = MongoClients.create(connection);
 
     public MongoDatabase getConnection() {
         cfg x = new cfg();
@@ -23,39 +23,72 @@ public class DB {
         return mongoClient.getDatabase("test");
     }
 
+    // connect to mongodb, returns a MongoClient
     public static MongoClient connect() {
-
-        return mongoClient;
+        return MongoClients.create(connection);
     }
 
+    // select the user collection, returns a MongoCollection
     public static MongoCollection<Document> getUserCollection() {
-        // select the user collection
+        MongoClient mongoClient = connect();
 
         return mongoClient.getDatabase("App").getCollection("users");
     }
 
-    public static void addUser(MongoCollection<Document> user, String name, String password) {
-        // add user to the user collection
-        Document doc = new Document("username", name).append("password", password);
-        user.insertOne(doc);
+    // adding a new user to database
+    public static void addUser(MongoCollection<Document> userCollection, String name, String ip, String password) {
+        // encrypt the password using simple hash
+        String encryptedPassword = Encryption.encryptPassword(password);
+        // check if user with given name exists
+        Document query = new Document("username", name);
+        FindIterable<Document> findIterable = userCollection.find(query);
+        MongoCursor<Document> cursor = findIterable.cursor();
+        if (cursor.hasNext()){
+            // if username is taken
+            System.out.println("Username already exists");
+        } else {
+            // add user to the user collection
+            Document doc = new Document("username", name).append("ip", ip).append("password", encryptedPassword);
+            userCollection.insertOne(doc);
+        }
     }
 
-    public static void login(MongoCollection<Document> userCollection, String username, String encryptedPassword) {
-        // find a user in the database
+    // method to log in
+    public static void login(MongoCollection<Document> userCollection, String username, String password) {
+        // encrypt the password using simple hash
+        String encryptedPassword = Encryption.encryptPassword(password);
+        // check if there is a user with the given username and password
         Document query = new Document("username", username).append("password", encryptedPassword);
         FindIterable<Document> findIterable = userCollection.find(query);
         MongoCursor<Document> cursor = findIterable.cursor();
         if (cursor.hasNext()){
-            System.out.println(cursor.next());
+            // if login is successful
+            //System.out.println(cursor.next());
+            System.out.println("Logged in as " + username);
         } else {
-            System.out.println("No user found");
+            // if login failed
+            System.out.println("Login Failed");
         }
     }
 
-    public static void deleteUser(MongoCollection<Document> userCollection, String username, String encryptedPassword) {
-        // delete user from database
+    public static void deleteUser(MongoCollection<Document> userCollection, String username, String password) {
+        // encrypt the password using simple hash
+        String encryptedPassword = Encryption.encryptPassword(password);
+        // find user with the given username and password
         Document query = new Document("username", username).append("password", encryptedPassword);
+        // delete user from database
         userCollection.deleteOne(query);
+        System.out.println(username + " has been deleted");
+    }
+
+    public static void changePassword(MongoCollection<Document> userCollection, String username, String password, String newPassword) {
+        // encrypt the password using simple hash
+        String encryptedPassword = Encryption.encryptPassword(password);
+        String encryptedNewPassword = Encryption.encryptPassword(newPassword);
+        // change password
+        Document query = new Document("username", username).append("password", encryptedPassword);
+        userCollection.findOneAndUpdate(query, Updates.set("password", encryptedNewPassword));
+        System.out.println("Changed password for " + username);
     }
 
 }
