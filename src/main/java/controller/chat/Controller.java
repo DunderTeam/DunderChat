fdd87186cde09d94d6d8c8967543912e748c8a92
Hans Kristian Hoel <hanskhoe@stud.ntnu.no>
2020-11-18 21:03:35 +0100

    merge

--- src/main/java/controller/chat/Controller.java
+++ src/main/java/controller/chat/Controller.java
@@ -1,8 +1,13 @@
 package controller.chat;
 
+import com.mongodb.client.MongoCollection;
 import model.networking.data.Message;
+import model.database.DB;
+import org.bson.Document;
 
 public class Controller {
+
+    MongoCollection<Document> Doc = DB.getUserCollection();
 
     public static void SendMessage(String sen, String mes, String add, String cName){ //ChatMsgSend
 
@@ -18,15 +23,19 @@ public class Controller {
         ChatManager.addChat(user, address, 5555);
     }
 
-    public void Login(){ // login user
+    public void Login(String Name, String Password){ // login user
 
+
+        DB.login(Doc, Name, Password); // log user inn to database
     }
 
     public void LogOut() { // log out user
 
     }
 
-    public void RegisterUser(){ // register new user for app
+    public void RegisterUser(String Name, String Password, String Ip ){ // register new user for app
+
+        DB.addUser(Doc, Name, Ip, Password ); // adds new user to Database
 
     }
 
@@ -34,12 +43,14 @@ public class Controller {
 
     }
 
-    public void ChangePassword(){ // Change password on user
+    public void ChangePassword(String Name, String OldPassword, String NewPassword){ // Change password on user
 
+        DB.changePassword(Doc, Name, OldPassword, NewPassword); // Change password on user
     }
 
-    public void DeleteUser(){ // Delete current user
+    public void DeleteUser(String Name, String Password){ // Delete current user
 
+        DB.deleteUser(Doc, Name, Password); // Delete user from database
     }
 
 
